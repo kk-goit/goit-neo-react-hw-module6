595ea311b55bce08c1e0477341a0baf4a4ed2587
Kostiantyn Kolomiichuk <kkolomiychuk@cleack.com>
2025-01-05 18:01:19 +0200

    Task complited

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,15 @@
-import { useState } from 'react'
-import './App.css'
+import ContactForm from './ContactForm/ContactForm'
+import SearchBox from './SearchBox/SearchBox'
+import ContactList from './ContactList/ContactList'
 
 function App() {
-
   return (
-    <>
-    </>
+    <div>
+      <h1>Phonebook</h1>
+      <ContactForm/>
+      <SearchBox/>
+      <ContactList/>
+    </div>
   )
 }
 
